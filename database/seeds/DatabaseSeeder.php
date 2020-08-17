844f02a9a42d66ef72cad245d021711d2c98620d
Binh Nguyen <nbinh1995@gmail.com>
2020-08-17 16:41:14 +0700

    deploy

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,29 +19,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $categories = ['Web Developer', 'Mobile Developer'];
-        // foreach ($categories as $item) {
-        //     Category::create(['name' => $item]);
-        // }
-        // factory(User::class, 20)->create();
+        $categories = ['Web Developer', 'Mobile Developer'];
+        foreach ($categories as $item) {
+            Category::create(['name' => $item]);
+        }
+        factory(User::class, 20)->create();
 
-        // User::where('role', 1)->each(function ($user, $key) {
-        //     factory(Company::class, 1)->create([
-        //         'user_id' => $user->id
-        //     ]);
-        // });
-        // User::where('role', 0)->each(function ($user, $key) {
-        //     factory(Profile::class, 1)->create([
-        //         'user_id' => $user->id,
-        //         'name' => $user->name
-        //     ]);
-        // });
-        // factory(Work::class, 20)->create();
+        User::where('role', 1)->each(function ($user, $key) {
+            factory(Company::class, 1)->create([
+                'user_id' => $user->id
+            ]);
+        });
+        User::where('role', 0)->each(function ($user, $key) {
+            factory(Profile::class, 1)->create([
+                'user_id' => $user->id,
+                'name' => $user->name
+            ]);
+        });
+        factory(Work::class, 20)->create();
 
 
-        // factory(Favorite::class, 20)->create();
+        factory(Favorite::class, 20)->create();
 
-        // factory(Applicant::class, 20)->create();
+        factory(Applicant::class, 20)->create();
         User::create([
             'name' => 'ntqb',
             'email' => 'ntqb@gmail.com',
