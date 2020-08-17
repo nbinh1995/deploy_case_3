07980b17f9732dc535cbad12c21452dd04ffe7d1
Binh Nguyen <nbinh1995@gmail.com>
2020-08-17 17:20:06 +0700

    deploy

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,8 +12,8 @@
     <!-- Spinner js -->
     <script src="{{asset('js/spinner.js')}}"></script>
     <!-- Fonts -->
-    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Yanone+Kaffeesatz" />
-    <link rel='stylesheet' href='//fonts.googleapis.com/css?family=Cantarell' type='text/css' />
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Yanone+Kaffeesatz" />
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Cantarell' type='text/css' />
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
         rel="stylesheet">
     <!-- Fonts Awesome -->
