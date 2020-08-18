f6dd7da14fdd836df8ebc923dc57ea54118637a2
Binh Nguyen <nbinh1995@gmail.com>
2020-08-18 19:22:17 +0700

    'deploy'

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Auth::routes();
+Auth::routes([
+    'register' => true,
+    'verify' => false,
+    'reset' => false
+]);
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/search', 'HomeController@search')->name('search');
 
